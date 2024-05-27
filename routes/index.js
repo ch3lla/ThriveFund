defff6edd282eff8fab1fb666bc160f248490a8e
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-05-27 13:04:24 +0100

    added upload middleware to upload route to allow file upload processing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { register, login } = require('../controllers/auth');
 const { verifyToken } = require('../utils/verifyToken');
+const upload = require('../utils/multerConfig');
 const { addFundRaisingDetails } = require('../controllers/applicant.controller');
 
 /**
@@ -75,6 +76,6 @@ router.post('/auth/register', register);
  */
 router.post('/auth/login', login);
 
-router.post('/upload', verifyToken, addFundRaisingDetails);
+router.post('/upload', verifyToken, upload.array('uploads'), addFundRaisingDetails);
 
 module.exports = router;
