dd7d6bfb19253298ec4c912b05cd59a4950bc870
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-05-30 15:34:41 +0100

    found why funding media was empty

--- middleware/media_upload.js
+++ middleware/media_upload.js
@@ -27,6 +27,14 @@ const processFileUpload = async function (req, res, next) {
       return;
     }
 
+    const fileKey = 'fundingMedia[file]'; // Adjust to match your form field name
+    const file = req.files[fileKey];
+
+    if (!file) {
+      res.status(400).json({ message: `No file uploaded under the name '${fileKey}'` });
+      return;
+    }
+
     const allowedMimeTypes = [
         "application/pdf",
         "image/jpeg",
@@ -37,7 +45,6 @@ const processFileUpload = async function (req, res, next) {
       console.log("req files - ", req.files);
       console.log("funding media - ", req.files.fundingMedia);
 
-      const file = req.files;
       const mimeType = file.mimetype || file.type;
 
       if (!allowedMimeTypes.includes(mimeType)) {
@@ -46,13 +53,13 @@ const processFileUpload = async function (req, res, next) {
       }
       
 
-    const buffer = req.files.fundingMedia.data;
+    const buffer = file.data;
     const result = await uploadStream(buffer);
 
     // update the req.body
     req.body = {
       ...req.body,
-      name: req.files.fundingMedia.name.toLowerCase(),
+      name: file.name.toLowerCase(),
       pathToFile: result.secure_url,
       publicId: result.public_id
     };
