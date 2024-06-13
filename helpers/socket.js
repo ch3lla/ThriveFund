34b7fa204e0f19a8e6c9197d97b13fca256c9fa5
Chukwuma Emmanuella <103493768+ch3lla@users.noreply.github.com>
2024-06-13 11:07:31 +0100

    fix: chnaged socket structure to run on app - Express

--- helpers/socket.js
+++ helpers/socket.js
@@ -1,20 +1,15 @@
-const http = require('http');
 const socketIO = require('socket.io');
 
-const server = http.createServer();
+let io;
 
-/* const io = socketIO(server, {
-  cors: {
-    origin: ['http://localhost:5173', 'https://thrivefund.vercel.app'],
-  },
-});
- */
-// Listen for WebSocket connections and handle events
-const handleConnection = (io) => {
+const startSocketServer = (server, corsOptions) => {
+  io = socketIO(server, {
+    cors: corsOptions,
+  });
+
   io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Handle disconnections
     socket.on('disconnect', () => {
       console.log('A user disconnected');
     });
@@ -22,29 +17,11 @@ const handleConnection = (io) => {
 };
 
 const notifySocketAfterSuccessfulPayment = (fundraiserId, newAmountRaised, donorName, donorAmount, anonimity) => {
-    if (anonimity){
-        io.emit('paymentReceived', { fundraiserId, newAmountRaised, donorAmount });
-    }
-  io.emit('paymentReceived', { fundraiserId, newAmountRaised, donorName, donorAmount });
+  if (anonimity) {
+    io.emit('paymentReceived', { fundraiserId, newAmountRaised, donorAmount });
+  } else {
+    io.emit('paymentReceived', { fundraiserId, newAmountRaised, donorName, donorAmount });
+  }
 };
-
-/* const startSocketServer = (port) => {
-  server.listen(port, () => {
-    console.log(`Socket server started on port ${port}`);
-  });
-};
- */
-const startSocketServer = (server) => {
-  const io = socketIO(server, {
-    cors: {
-      origin: '*', // Allow connections from any origin
-    },
-  });
-
-  handleConnection(io);
-
-  return io;
-};
-
 
 module.exports = { startSocketServer, notifySocketAfterSuccessfulPayment };
